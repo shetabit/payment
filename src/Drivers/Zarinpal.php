d95162175ef7925109a9e7c2d94a395db38599a1
mahdi khanzadi <khanzadimahdi@gmail.com>
2019-02-18 10:24:49 +0330

    add driver error translations

--- src/Drivers/Zarinpal.php
+++ src/Drivers/Zarinpal.php
@@ -102,6 +102,7 @@ class Zarinpal extends Driver
      *
      * @return mixed|void
      * @throws InvalidPaymentException
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function verify()
     {
@@ -118,11 +119,39 @@ class Zarinpal extends Driver
         );
         $body = json_decode($response->getBody()->getContents(), true);
 
-        // throw an exception when payment has some issues!
-        if ($body['Status'] == 101) {
-            throw new InvalidPaymentException('payment has been verified before');
+        if (!isset($body['Status']) || $body['Status'] != 100) {
+            $this->notVerified($body['Status']);
+        }
+    }
+
+    /**
+     * Trigger an exception
+     *
+     * @param $status
+     * @throws InvalidPaymentException
+     */
+    private function notVerified($status) {
+        $translations = array(
+            "-1" => "اطلاعات ارسال شده ناقص است.",
+            "-2" => "IP و يا مرچنت كد پذيرنده صحيح نيست",
+            "-3" => "با توجه به محدوديت هاي شاپرك امكان پرداخت با رقم درخواست شده ميسر نمي باشد",
+            "-4" => "سطح تاييد پذيرنده پايين تر از سطح نقره اي است.",
+            "-11" => "درخواست مورد نظر يافت نشد.",
+            "-12" => "امكان ويرايش درخواست ميسر نمي باشد.",
+            "-21" => "هيچ نوع عمليات مالي براي اين تراكنش يافت نشد",
+            "-22" => "تراكنش نا موفق ميباشد",
+            "-33" => "رقم تراكنش با رقم پرداخت شده مطابقت ندارد",
+            "-34" => "سقف تقسيم تراكنش از لحاظ تعداد يا رقم عبور نموده است",
+            "-40" => "اجازه دسترسي به متد مربوطه وجود ندارد.",
+            "-41" => "اطلاعات ارسال شده مربوط به AdditionalData غيرمعتبر ميباشد.",
+            "-42" => "مدت زمان معتبر طول عمر شناسه پرداخت بايد بين 30 دقيه تا 45 روز مي باشد.",
+            "-54" => "درخواست مورد نظر آرشيو شده است",
+            "101" => "عمليات پرداخت موفق بوده و قبلا PaymentVerification تراكنش انجام شده است.",
+        );
+        if (array_key_exists($status, $translations)) {
+            throw new InvalidPaymentException($translations[$status]);
         } else {
-            throw new InvalidPaymentException($body['errors']);
+            throw new InvalidPaymentException('خطای ناشناخته ای رخ داده است.');
         }
     }
 }
