eda67e3b92e10feac06b1f84c0738b83b7fd3f68
awebmaker <mosaviali701@gmail.com>
2020-05-03 11:28:23 +0430

    Debug Melat Paymen And True Payment

--- src/Drivers/Behpardakht/Behpardakht.php
+++ src/Drivers/Behpardakht/Behpardakht.php
@@ -48,6 +48,7 @@ class Behpardakht extends Driver
      */
     public function purchase()
     {
+        error_reporting(0);
         $soap = new \SoapClient($this->settings->apiPurchaseUrl);
         $response = $soap->bpPayRequest($this->preparePurchaseData());
 
@@ -56,7 +57,10 @@ class Behpardakht extends Driver
             throw new PurchaseFailedException('پذیرنده معتبر نیست.');
         }
 
-        $data = explode(',', $response);
+
+        $data = explode(',', $response->return);
+
+
 
         // purchase was not successful
         if ($data[0] != "0") {
@@ -107,7 +111,7 @@ class Behpardakht extends Driver
         $soap = new \SoapClient($this->settings->apiVerificationUrl);
 
         // step1: verify request
-        $verifyResponse = $soap->bpVerifyRequest($data, $this->settings->apiNamespaceUrl);
+        $verifyResponse = (int)$soap->bpVerifyRequest($data, $this->settings->apiNamespaceUrl)->return;
         if ($verifyResponse != 0) {
             // rollback money and throw exception
             $soap->bpReversalRequest($data, $this->settings->apiNamespaceUrl);
@@ -115,7 +119,7 @@ class Behpardakht extends Driver
         }
 
         // step2: settle request
-        $settleResponse = $soap->bpSettleRequest($data, $this->settings->apiNamespaceUrl);
+        $settleResponse = $soap->bpSettleRequest($data, $this->settings->apiNamespaceUrl)->return;
         if ($settleResponse != 0) {
             // rollback money and throw exception
             $soap->bpReversalRequest($data, $this->settings->apiNamespaceUrl);
