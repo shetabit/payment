bc76357868256a3d7bc5dc77b8db972253d038b7
Ali Khosravi <alikhosravi175@gmail.com>
2023-03-15 15:00:12 +0330

    Added phpdoc to Payment facade class for IDE auto completion (#274)

--- src/Facade/Payment.php
+++ src/Facade/Payment.php
@@ -3,11 +3,24 @@
 namespace Shetabit\Payment\Facade;
 
 use Illuminate\Support\Facades\Facade;
+use Shetabit\Multipay\Invoice;
 
 /**
  * Class Payment
  *
+ * @method static config($key, $value = null)
+ * @method static callbackUrl($url = null)
+ * @method static resetCallbackUrl()
+ * @method static amount($amount)
+ * @method static detail($key, $value = null)
+ * @method static transactionId($id)
+ * @method static via($driver)
+ * @method static purchase(Invoice $invoice = null, $finalizeCallback = null)
+ * @method static pay($initializeCallback = null)
+ * @method static verify($finalizeCallback = null)
+ *
  * @package Shetabit\Payment\Facade
+ * @see \Shetabit\Multipay\Payment
  */
 class Payment extends Facade
 {
